58354da9b0e74719155ec85ad7711c046fbe5421
Noer Fahmie <noer.fahmie@gmail.com>
2026-04-19 02:17:39 +0700

    Rename Affiliates to Account Details, MY admin read-only, ID admin can edit

--- src/routes/affiliates.js
+++ src/routes/affiliates.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const affiliateController = require('../controllers/affiliateController');
-const { requireAuth, requireStudioOrUp } = require('../middleware/auth');
+const { requireAuth, requireStudioOrUp, requireISHAdmin } = require('../middleware/auth');
 
 router.use(requireAuth, requireStudioOrUp);
 
+// View (all roles)
 router.get('/', affiliateController.index);
-router.get('/create', affiliateController.getCreate);
-router.post('/create', affiliateController.postCreate);
-router.get('/:id/edit', affiliateController.getEdit);
-router.post('/:id/edit', affiliateController.postEdit);
-router.post('/:id/delete', affiliateController.postDelete);
+
+// Edit/Create/Delete (ISH admin only = superadmin + indonesia_admin)
+router.get('/create', requireISHAdmin, affiliateController.getCreate);
+router.post('/create', requireISHAdmin, affiliateController.postCreate);
+router.get('/:id/edit', requireISHAdmin, affiliateController.getEdit);
+router.post('/:id/edit', requireISHAdmin, affiliateController.postEdit);
+router.post('/:id/delete', requireISHAdmin, affiliateController.postDelete);
 
 module.exports = router;
